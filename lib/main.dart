abd6792db23b7e3145dea189a9732b2aa2cec647
pzhangleo <pengzh.leo@gmail.com>
2019-03-28 17:52:14 +0800

    将部分移动到文件夹中

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:flutter_app/route.dart';
+import 'package:flutter_app/route/route.dart';
 
 void main() => runApp(MyApp());
 
